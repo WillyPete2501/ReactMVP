bbb336767a7e3f89368f009b00d6eb592bf99c02
WillyPete2501 <108033885+WillyPete2501@users.noreply.github.com>
2022-10-03 16:07:29 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import TaskInput from "./components/TaskInput";
+import ListTask from "./components/ListTask";
+
+function App() {
+  return (
+      <div className="container">
+        <TaskInput />
+        <ListTask />
+      </div>
+  );
+}
+
+export default App;
