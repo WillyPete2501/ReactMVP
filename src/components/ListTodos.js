b4c5c6e8dca8669b41257c978b618dafc46b69fd
WillyPete2501 <108033885+WillyPete2501@users.noreply.github.com>
2022-10-03 15:41:48 -0700

    Add files via upload

---
+++ src/components/ListTodos.js
@@ -0,0 +1,69 @@
+import React, {useEffect, useState} from "react";
+import EditTodo from "./EditTodo";
+
+const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  const deleteTodo = async id => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:3350/task/${id}`, {
+        method: "DELETE"
+      });
+
+      setTodos(todos.filter(todo => todo.task_id !== id));
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:3350/task");
+      const jsonData = await response.json();
+
+      setTodos(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  console.log(todos);
+
+  return (
+    <div>
+      <table className="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map(todo => (
+            <tr key={todo.task_id}>
+              <td>{todo.task}</td>
+              <td>
+                <EditTodo todo={todo} />
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.task_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ListTodos;
